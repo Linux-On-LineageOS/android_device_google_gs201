8443eb1482b947a61ed1f87c0afe2e80dd61dd07
Archie Meng <archiemeng@protonmail.com>
2025-11-03 21:09:59 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -1021,3 +1021,4 @@ include device/google/gs-common/touch/twoshay/twoshay.mk
 # since it can't be overridden from /vendor.
 PRODUCT_PRODUCT_PROPERTIES += \
 	dumpstate.strict_run=false
+$(call inherit-product, vendor/lindroid/lindroid.mk)
